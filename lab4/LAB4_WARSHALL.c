3aae0863e7aa6a28e42080b8c974ce0f66ff0d03
lakshya1216 <lakshya.cs23@bmsce.ac.in>
2025-04-08 10:19:54 +0530

    Add files via upload

---
+++ lab4/LAB4_WARSHALL.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#define MAX 5
+
+ void Warshall(int graph[][MAX],int n)
+ {
+     for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                    graph[i][j] = graph[i][j] || graph[i][k] && graph[k][j];
+            }
+        }
+     }
+ }
+
+ void main()
+ {
+     int graph[MAX][MAX],n;
+     printf("Enter the number of Vertices\n");
+     scanf("%d",&n);
+     printf("Enter the Cost Adjacency Matrix\n");
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&graph[i][j]);
+        }
+     }
+     Warshall(graph,n);
+     printf("Shortest Path Matrix\n");
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d\t",graph[i][j]);
+        }
+        printf("\n");
+     }
+ }
